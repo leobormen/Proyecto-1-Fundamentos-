412e0db1098754bf3b81cfc621613987f498dc61
leobormen <leo.borbon.mena@gmail.com>
2026-04-23 09:57:18 -0600

    Add files via upload

---
+++ Pruebas_proyecto.py
@@ -0,0 +1,162 @@
+import tkinter as tk
+from tkinter import *
+import keyboard
+import time
+import random
+import socket
+import threading
+
+SERVER_IP = "10.165.83.130"  # Cambia esto con la IP de la Pico W
+PORT = 1717
+
+Morse_diccionario = {"a": [0, 2, 1], "b": [1, 2, 0, 2, 0, 2, 0], "c": [1, 2, 0, 2, 1, 2, 0], "d": [1, 2, 0, 2, 0], "e": [0], "f": [0, 2, 0, 2, 1, 2, 0],
+         "g": [1, 2, 1, 2, 0], "h": [0, 2, 0, 2, 0, 2, 0], "i": [0, 2, 0], "j": [0, 2, 1, 2, 1, 2, 1], "k": [1, 2, 0, 2, 1], "l": [0, 2, 1, 2, 0, 2, 0], 
+         "m": [1, 2, 1], "n": [1, 2, 0], "ñ": [1, 2, 1, 2, 0, 2, 1, 2, 1], "o": [1, 2, 1, 2, 1],"p": [0, 2, 1, 2, 1, 2, 0], "q": [1, 2, 1, 2, 0, 2, 1], 
+         "r": [0, 2, 1, 2, 0], "s": [0, 2, 0, 2, 0], "t": [1], "u": [0, 2, 0, 2, 1], "v": [0, 2, 0, 2, 0, 2, 1], "w": [0, 2, 1, 2, 1], "x": [1, 2, 0, 2, 0, 2, 1], 
+         "y": [1, 2, 0, 2, 1, 2, 1], "z": [1, 2, 1, 2, 0, 2, 0], "0": [1, 2, 1, 2, 1, 2, 1, 2, 1], "1": [0, 2, 1, 2, 1, 2, 1, 2, 1], 
+         "2": [0, 2, 0, 2, 1, 2, 1, 2, 1], "3": [0, 2, 0, 2, 0, 2, 1, 2, 1], "4": [0, 2, 0, 2, 0, 2, 0, 2, 1], "5": [0, 2, 0, 2, 0, 2, 0, 2, 0], 
+         "6": [1, 2, 0, 2, 0, 2, 0, 2, 0], "7": [1, 2, 1, 2, 0, 2, 0, 2, 0], "8": [1, 2, 1, 2, 1, 2, 0, 2, 0], "9": [1, 2, 1, 2, 1, 2, 1, 2, 0],
+         "+": [0, 2, 1, 2, 0, 2, 1, 2, 0], "-": [1, 2, 0, 2, 0, 2, 0, 2, 0, 2, 1]}
+
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def connect():
+    try:
+        client_socket.connect((SERVER_IP, PORT))
+        threading.Thread(target=receive_messages, daemon=True).start()
+        status_label.config(text="Conectado al servidor")
+    except Exception as e:
+        status_label.config(text=f"Error: {e}")
+
+
+def receive_messages():
+    global puntaje2
+    while True:
+        try:
+            msg = client_socket.recv(1024).decode()
+            puntaje2 += finalizar(list(msg))
+            print(puntaje2)
+            Puntaje2_Label["text"] = puntaje2, "/", len(palabra_morse)
+        except:
+            break
+
+def salir():
+    try:
+        client_socket.close()
+    except:
+        pass
+    window.destroy()
+
+
+def traducir(palabra):
+    global palabra_morse
+    for i in range(len(palabra)):
+        palabra_morse += Morse_diccionario[palabra[i].lower()]
+        if palabra[i] != " " and i != (len(palabra)-1):
+            if palabra[i+1] == " ":
+                palabra_morse += [4]
+            else:
+                palabra_morse += [3]
+    print(palabra_morse)
+
+
+def finalizar(lista):  #Finaliza la interpretacion
+    puntaje = 0
+    print(lista) 
+    for i in range (len(palabra_morse[:len(lista)])): # Verifica que tan correcto está el input del jugador, notar que se corta la lista prehecha para acomodar a cuanto puso el usuario
+        if palabra_morse[i] == int(lista[i]):
+            puntaje += 1
+    print(puntaje)
+    Texto_puntaje["text"] = "Tu puntaje es"
+    return puntaje
+    
+
+def contar():  #Esto interpreta como el usuario presiona el boton indicado, define cual numero poner.
+    conteo = 0
+    global Puntaje1_Label
+    global puntaje1
+    while keyboard.is_pressed("a"):
+        conteo += 0.01
+        time.sleep(0.01)
+    if conteo <= 0.30:
+        lista_jugador_1.append(0) # 0 indica dot
+    elif conteo > 0.30:
+        lista_jugador_1.append(1) # 1 indica dash
+    conteo = 0
+    while conteo <= 1:
+        conteo += 0.01
+        time.sleep(0.01)
+        if keyboard.is_pressed("a"):
+            break
+    if conteo <= 0.30:
+        lista_jugador_1.append(2) # 2 indica espacio entre dash o dot
+    elif 0.30 < conteo <= 0.70:
+        lista_jugador_1.append(3) # 3 indica espacio entre letras
+    elif 0.70 < conteo <= 1:
+        lista_jugador_1.append(4) # 4 indica espacio entre palabras
+    else:
+        puntaje1 += finalizar(lista_jugador_1)
+        Puntaje1_Label["text"] = puntaje1, "/", len(palabra_morse)
+        lista_jugador_1.clear()
+
+def pasar_a_string(lista):
+    string_palabra = ""
+    for x in lista:
+        string_palabra += str(x)
+    return string_palabra
+
+    
+def empezar_juego_1():
+    global puntaje1
+    global puntaje2
+    global palabra_morse
+    Texto_puntaje["text"] = ""
+    Puntaje1_Label["text"] = ""
+    Puntaje2_Label["text"] = ""
+    puntaje2 = 0
+    puntaje1 = 0
+    palabra_morse.clear()
+    palabra_elegida = Lista_palabras[random.randint(0,9)]
+    traducir(palabra_elegida)
+    msg = pasar_a_string(palabra_morse)
+    if msg:
+        client_socket.send(msg.encode())
+
+
+Lista_palabras = ["Bomba", "Capybara", "Chocolate", "Gatico", "Mondongo", "Esencia", "1984", "qwerty", "odio el lol", "i like trains", "Buenos dias"]
+
+
+palabra_morse= []
+lista_jugador_1 = []
+puntaje1 = 0
+puntaje2 = 0
+
+window = tk.Tk()
+window.geometry("400x400")
+
+
+boton = tk.Button(window, text= "iniciar juego 1", command= empezar_juego_1)
+boton.place(x= 100, y= 0)
+
+Texto_puntaje = tk.Label(window, text="")
+Texto_puntaje.place(x= 100, y= 150)
+
+Puntaje1_Label = tk.Label(window, text= "hola")
+Puntaje1_Label.place(x= 200, y= 225)
+
+Puntaje2_Label = tk.Label(window, text= "hola")
+Puntaje2_Label.place(x= 300, y= 225)
+
+status_label = tk.Label(window, text="Desconectado")
+status_label.place(x= 100, y= 300)
+
+salir_btn = tk.Button(window, text="Salir", command=salir)
+salir_btn.place(x= 100, y= 350)
+
+
+keyboard.add_hotkey('a', contar)
+
+
+connect()
+window.mainloop()   
