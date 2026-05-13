fe5a82596e2d60d9ee278d0519916c8e144fe6cb
leobormen <leo.borbon.mena@gmail.com>
2026-05-12 22:20:13 -0600

    Add files via upload

--- Pruebas_proyecto.py
+++ Pruebas_proyecto.py
@@ -21,11 +21,19 @@ Morse_diccionario = {"a": [0, 2, 1], "b": [1, 2, 0, 2, 0, 2, 0], "c": [1, 2, 0, 
 
 puntajes = []
 
-with open("puntajes.txt", "r") as file:
-    puntajes_str = file.read()
-    puntajes = puntajes_str.split(",")
+
+
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def resultados():
+    puntuacion_jugador_1["text"] = "Puntaje del jugador 1: ", puntaje1
+    puntuacion_jugador_2["text"] = "Puntaje del jugador 2: ", puntaje2
+    window_resultados.place(x=0,y=0)
+    if puntaje1 > puntaje2:
+        ganador_label["text"] = "Ha ganado el jugador 1"
+    else:
+        ganador_label["text"] = "Ha ganado el jugador 2"
 
 def connect():
     try:
@@ -42,11 +50,16 @@ def receive_messages():
             msg = client_socket.recv(1024).decode()
             puntaje2 += finalizar(list(msg))
             print(puntaje2)
-            Puntaje2_Label["text"] = finalizar(list(msg)), "/", len(palabra_morse)
-            if rondas == 3:
-                finalizar_juego2.place(y=350, x=200)
+            if rondas % 2 == 0:
+                puntaje1 += finalizar(lista_jugador_1)
+                Puntaje1_Label["text"] = finalizar(lista_jugador_1), "/", len(palabra_morse)
             else:
-                ronda_siguiente_juego2.place(y=350,x=200)
+                puntaje2 += finalizar(lista_jugador_1)
+                Puntaje2_Label["text"] = finalizar(lista_jugador_1), "/", len(palabra_morse)
+            if rondas == 3:
+                finalizar_juego2.place(y=350, x=160) 
+            else:
+                ronda_siguiente_juego2.place(y=350,x=160)
         except:
             break
 
@@ -80,6 +93,7 @@ def ver_puntuaciones():
 def menu():
     finalizar_juego1.place_forget()
     finalizar_juego2.place_forget()
+    window_resultados.place_forget()
     window_puntuaciones.place_forget()
     window_juego_1.place_forget()
     window_juego_2.place_forget()
@@ -106,20 +120,25 @@ def traducir(palabra):
 def comparar_entrada():
     global palabra_elegida
     global puntaje_juego1
-    Palabra_respuesta = Respuesta.get()
-    for i in range(len(Palabra_respuesta[:len(palabra_elegida)])):
-        if Palabra_respuesta[i].lower() == palabra_elegida[i].lower():
-            puntaje_juego1 += 1
-    Puntaje_juego1_Label["text"] = ("Puntaje: ", puntaje_juego1)
-    if rondas == 3:
-        puntaje()
-        finalizar_juego1.place(y=350, x=200)
-    else:
-        ronda_siguiente_juego1.place(y=350,x=200)
+    global adivinando
+    if adivinando:
+        adivinando = False
+        Palabra_respuesta = Respuesta.get()
+        for i in range(len(Palabra_respuesta[:len(palabra_elegida)])):
+            if Palabra_respuesta[i].lower() == palabra_elegida[i].lower():
+                puntaje_juego1 += 1
+        Puntaje_juego1_Label["text"] = ("Puntaje: ", puntaje_juego1)
+        if rondas == 3:
+            puntaje()
+            finalizar_juego1.place(y=350, x=160)
+        else:
+            ronda_siguiente_juego1.place(y=350,x=160)
 
 def siguiente_ronda_juego2():
+    global adivinando
     global palabra_elegida
     global rondas
+    adivinando = True
     rondas += 1
     ronda_siguiente_juego2.place_forget()
     palabra_morse.clear()
@@ -134,8 +153,10 @@ def siguiente_ronda_juego2():
         client_socket.send(msg.encode())
 
 def siguiente_ronda_juego1():
+    global adivinando
     global palabra_elegida
     global rondas
+    adivinando = True
     rondas += 1
     ronda_siguiente_juego1.place_forget()
     Respuesta.delete(0, tk.END)
@@ -154,13 +175,15 @@ def finalizar(lista):  #Finaliza la interpretacion
         if palabra_morse[i] == int(lista[i]):
             puntaje += 1
     print(puntaje)
-    Texto_puntaje["text"] = "Tu puntaje es"
+    Texto_puntaje["text"] = "Tu puntaje es:"
     return puntaje
     
 def contar():  #Esto interpreta como el usuario presiona el boton indicado, define cual numero poner.
     conteo = 0
     global Puntaje1_Label
     global puntaje1
+    global puntaje2
+    global adivinando
     while keyboard.is_pressed("space"):
         conteo += 0.01
         time.sleep(0.01)
@@ -181,10 +204,19 @@ def contar():  #Esto interpreta como el usuario presiona el boton indicado, defi
     elif 0.70 < conteo <= 1:
         lista_jugador_1.append(4) # 4 indica espacio entre palabras
     else:
-        puntaje1 += finalizar(lista_jugador_1)
-        Puntaje1_Label["text"] = finalizar(lista_jugador_1), "/", len(palabra_morse)
+        if rondas % 2 == 1:
+            puntaje1 += finalizar(lista_jugador_1)
+            Puntaje1_Label["text"] = finalizar(lista_jugador_1), "/", len(palabra_morse)
+            adivinando = False
+        else:
+            puntaje2 += finalizar(lista_jugador_1)
+            Puntaje2_Label["text"] = finalizar(lista_jugador_1), "/", len(palabra_morse)
+            adivinando = False
+        if rondas == 3:
+            finalizar_juego2.place(y=350, x=200) 
+        else:
+            ronda_siguiente_juego2.place(y=350,x=200)
         lista_jugador_1.clear()
-        ronda_siguiente_juego2.place(y=350,x=200)
 
 def pasar_a_string(lista):
     string_palabra = ""
@@ -197,6 +229,8 @@ def empezar_juego_1():
     global puntaje_juego1
     global palabra_morse
     global rondas
+    global adivinando
+    adivinando = True
     rondas = 1
     window_main.place_forget()
     record_label.place_forget()
@@ -217,6 +251,8 @@ def empezar_juego_2():
     global puntaje2
     global palabra_morse
     global rondas
+    global adivinando
+    adivinando = True
     rondas = 1
     Texto_puntaje["text"] = ""
     Puntaje1_Label["text"] = ""
@@ -243,6 +279,7 @@ puntaje2 = 0
 puntaje_total_1 = 0
 puntaje_total_2 = 0
 rondas = 1
+adivinando = True
 
 #--------------------------------Parte principal de la interfaz-----------------------------
 
@@ -253,98 +290,118 @@ window.resizable(False,False)
 
 
 
-window_main = tk.Canvas(window, height= 400, width= 400)
+window_main = tk.Canvas(window, height= 400, width= 400, bg="#a52a2a")
 window_main.place(x=0,y=0)
 
-menuseleccion=tk.Canvas(window_main, height=200, width =200, bg='grey')
-menuseleccion.place(x=0)
+titulo=tk.Label(window_main, text = ('Proyecto Código Morse'), font=('Papyrus',20),fg="#ffffff", bg="#a52a2a")
+titulo.place(x=50, y=25)
 
-titulo=tk.Label(window_main, text = ('Proyecto Código Morse'), font=('Papyrus',20),fg='blue')
-titulo.place(x=75, y=100)
+boton_juego1 = tk.Button(window_main, text= "Iniciar Juego 1", command= empezar_juego_1)
+boton_juego1.place(x=90,y=100)
 
-
-boton_juego1 = tk.Button(menuseleccion, text= "Iniciar Juego 1", command= empezar_juego_1)
-boton_juego1.pack()
-
-boton_juego2 = tk.Button(menuseleccion, text= "Iniciar Juego 2", command= empezar_juego_2)
-boton_juego2.pack()
+boton_juego2 = tk.Button(window_main, text= "Iniciar Juego 2", command= empezar_juego_2)
+boton_juego2.place(x=225,y=100)
 
 boton_puntuaciones = tk.Button(window_main, text= "HIGHSCORES", command= ver_puntuaciones)
 boton_puntuaciones.place(x= 160, y= 200)
 
-#----------------------------------Ventana del juego 2----------------------------------------
-
-window_juego_2 = tk.Canvas(window, width=400,height=400)
-
-Texto_puntaje = tk.Label(window_juego_2, text="")
-Texto_puntaje.place(x= 100, y= 150)
-
-Puntaje1_Label = tk.Label(window_juego_2, text= "hola")
-Puntaje1_Label.place(x= 200, y= 225)
-
-Puntaje2_Label = tk.Label(window_juego_2, text= "hola")
-Puntaje2_Label.place(x= 300, y= 225)
-
-status_label = tk.Label(window_main, text="Desconectado")
+status_label = tk.Label(window_main, text="Desconectado", fg="#ffffff", bg="#a52a2a")
 status_label.place(x= 160, y= 300)
 
 salir_btn = tk.Button(window_main, text="Salir", command=salir)
-salir_btn.place(x= 175, y= 350)
+salir_btn.place(x= 185, y= 350)
+
+#----------------------------------Ventana del juego 2----------------------------------------
+
+window_juego_2 = tk.Canvas(window, width=400,height=400, bg="#a52a2a")
+
+juego2_Label = tk.Label(window_juego_2, text="Modo Escucha", font=('Papyrus',20), bg="#a52a2a",fg="#ffffff")
+juego2_Label.place(x=100, y=20)
+
+Texto_puntaje = tk.Label(window_juego_2, text="", bg="#a52a2a",fg="#ffffff")
+Texto_puntaje.place(x= 150, y= 125)
+
+Puntaje1_Label = tk.Label(window_juego_2, text= "hola", bg="#a52a2a",fg="#ffffff")
+Puntaje1_Label.place(x= 100, y= 200)
+
+Puntaje2_Label = tk.Label(window_juego_2, text= "hola", bg="#a52a2a",fg="#ffffff")
+Puntaje2_Label.place(x= 250, y= 200)
+
+
 
 ronda_siguiente_juego2 = tk.Button(window_juego_2, text= "Siguiente ronda", command=siguiente_ronda_juego2)
-finalizar_juego2 = tk.Button(window_juego_2, text= "Finalizar", command=menu)
+finalizar_juego2 = tk.Button(window_juego_2, text= "Finalizar", command=resultados)
 
 #-----------------------------------Ventana del juego 1------------------------------------
 
-window_juego_1 = tk.Canvas(window, height=400, width=400)
+window_juego_1 = tk.Canvas(window, height=400, width=400, bg="#a52a2a")
+
+juego1_Label = tk.Label(window_juego_1, text="Modo Transmisión Simple", font=('Papyrus',20), bg="#a52a2a",fg="#ffffff")
+juego1_Label.place(x=50, y=20)
 
 Respuesta = tk.Entry(window_juego_1)
-Respuesta.place(x=10,y=150)
+Respuesta.place(x=140,y=150)
 
 boton_respuesta = tk.Button(window_juego_1, text= "Confirmar", command=comparar_entrada)
-boton_respuesta.place(x=200, y= 250)
+boton_respuesta.place(x=170, y= 250)
 
 ronda_siguiente_juego1 = tk.Button(window_juego_1, text= "Siguiente ronda", command=siguiente_ronda_juego1)
 finalizar_juego1 = tk.Button(window_juego_1, text= "Finalizar", command=menu)
 
-Puntaje_juego1_Label = tk.Label(window_juego_1, text= "")
-Puntaje_juego1_Label.place(x=200,y=300)
+Puntaje_juego1_Label = tk.Label(window_juego_1, text= "", bg="#a52a2a",fg="#ffffff")
+Puntaje_juego1_Label.place(x=170,y=300)
 
-record_label = tk.Label(window_juego_1, text= "¡Nuevo record!")
+record_label = tk.Label(window_juego_1, text= "¡Nuevo record!", bg="#a52a2a",fg="#ffffff")
+
+#-------------------------------------------Ventana de resultados Juego 2-----------------------------------------------
+
+window_resultados = tk.Canvas(window, width=400, height=400, bg="#a52a2a")
+
+ganador_label = tk.Label(window_resultados, text="", bg="#a52a2a",fg="#ffffff", font=('Papyrus',14))
+ganador_label.place(x=100, y=300)
+
+resultados_label = tk.Label(window_resultados, text="Resultados", font=('Papyrus',20), bg="#a52a2a",fg="#ffffff")
+resultados_label.place(x=130, y=20)
+
+puntuacion_jugador_1 = tk.Label(window_resultados, text= "", bg="#a52a2a",fg="#ffffff", font=('Papyrus',14))
+puntuacion_jugador_1.place(x= 90, y= 100)
+puntuacion_jugador_2 = tk.Label(window_resultados, text= "", bg="#a52a2a",fg="#ffffff", font=('Papyrus',14))
+puntuacion_jugador_2.place(x= 90, y= 200)
+
+boton_menu_resultados = tk.Button(window_resultados, text="Regresar", command=menu)
+boton_menu_resultados.place(x=10, y= 10)
 
 #---------------------------------Ventana de puntuaciones---------------------------------------------------------------
 
-window_puntuaciones = tk.Canvas(window, height=400, width=400)
+window_puntuaciones = tk.Canvas(window, height=400, width=400, bg="#a52a2a")
 
-puntuacion_label = tk.Label(window_puntuaciones, text="EL TOP 10")
+puntuacion_label = tk.Label(window_puntuaciones, text="EL TOP 10", font=('Papyrus',20), bg="#a52a2a",fg="#ffffff")
 puntuacion_label.place(x= 150, y= 10)
-puntuacion1_label = tk.Label(window_puntuaciones, text="", width= 32)
+puntuacion1_label = tk.Label(window_puntuaciones, text="", width= 32, bg="#a52a2a",fg="#ffffff")
 puntuacion1_label.place(x=150, y= 40)
-puntuacion2_label = tk.Label(window_puntuaciones, text="", width= 32)
+puntuacion2_label = tk.Label(window_puntuaciones, text="", width= 32, bg="#a52a2a",fg="#ffffff")
 puntuacion2_label.place(x=150, y= 70)
-puntuacion3_label = tk.Label(window_puntuaciones, text="", width= 32)
+puntuacion3_label = tk.Label(window_puntuaciones, text="", width= 32, bg="#a52a2a",fg="#ffffff")
 puntuacion3_label.place(x=150, y= 100)
-puntuacion4_label = tk.Label(window_puntuaciones, text="", width= 32)
+puntuacion4_label = tk.Label(window_puntuaciones, text="", width= 32, bg="#a52a2a",fg="#ffffff")
 puntuacion4_label.place(x=150, y= 130)
-puntuacion5_label = tk.Label(window_puntuaciones, text="", width= 32)
+puntuacion5_label = tk.Label(window_puntuaciones, text="", width= 32, bg="#a52a2a",fg="#ffffff")
 puntuacion5_label.place(x=150, y= 160)
-puntuacion6_label = tk.Label(window_puntuaciones, text="", width= 32)
+puntuacion6_label = tk.Label(window_puntuaciones, text="", width= 32, bg="#a52a2a",fg="#ffffff")
 puntuacion6_label.place(x=150, y= 190)
-puntuacion7_label = tk.Label(window_puntuaciones, text="", width= 32)
+puntuacion7_label = tk.Label(window_puntuaciones, text="", width= 32, bg="#a52a2a",fg="#ffffff")
 puntuacion7_label.place(x=150, y= 220)
-puntuacion8_label = tk.Label(window_puntuaciones, text="", width= 32)
+puntuacion8_label = tk.Label(window_puntuaciones, text="", width= 32, bg="#a52a2a",fg="#ffffff")
 puntuacion8_label.place(x=150, y= 250)
-puntuacion9_label = tk.Label(window_puntuaciones, text="", width= 32)
+puntuacion9_label = tk.Label(window_puntuaciones, text="", width= 32, bg="#a52a2a",fg="#ffffff")
 puntuacion9_label.place(x=150, y= 280)
-puntuacion10_label = tk.Label(window_puntuaciones, text="", width= 32)
+puntuacion10_label = tk.Label(window_puntuaciones, text="", width= 32, bg="#a52a2a",fg="#ffffff")
 puntuacion10_label.place(x=150, y= 310)
 
 boton_menu = tk.Button(window_puntuaciones, text="Regresar", command=menu)
 boton_menu.place(x=10, y= 10)
 
-
 keyboard.add_hotkey('space', contar)
 
 
-connect()
 window.mainloop()   
